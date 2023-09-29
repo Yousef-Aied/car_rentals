47e4c359c6d50bbeed1a4e31b01a895cb0b35565
Yousef Aied <yousefaied71@gmail.com>
2023-09-29 21:26:14 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Footer, Navbar } from '@/components'
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+
+export const metadata: Metadata = {
+  title: 'Car Hub',
+  description: 'Discover the best cars in the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relatiove">
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+    </html>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['cdn.imagin.studio']
+    }
+}
+
+module.exports = nextConfig
+
